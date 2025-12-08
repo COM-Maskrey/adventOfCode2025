b521b0a44886847fd129e26e1a4db113bfc3e0ab
COM-Maskrey <michael@maskrey.net>
2025-12-08 13:47:17 +0000

    4.2

--- Day 4/Part 2/day4_2.py
+++ Day 4/Part 2/day4_2.py
@@ -1,66 +1,87 @@
 try:
     countOfAccessibleRolls = 0
-    previousRow = ''
-    currentRow = ''
-    nextRow = ''
+    totalPasses = 0
     rowNumber = 1
-    with open('Day 4/Part 2/Day_4_2_input.csv', 'r') as inputFile:
+    allTheRolls = []
+    with open('Day 4/Part 2/Day_4_2_Input.csv', 'r') as inputFile:
+        # 1 - Load all rolls locations into an array
         while True:
             rowOfRolls = inputFile.readline().strip("\n")
+            if rowNumber == 1:
+                allTheRolls.append('.' * (len(rowOfRolls)))
+                rowNumber += 1
             if rowOfRolls == '':
-                nextRow = '.' * len(currentRow)
-            else:
-                nextRow = rowOfRolls
-            if currentRow == '':
-                previousRow = '.' * len(rowOfRolls)
-                currentRow = rowOfRolls
-                continue
-            for index in range(len(currentRow)):
-                if currentRow[index] == '.':
-                    continue
-                countRollsAround = 0
-                for count in range(8):
-                    match count:
-                        case 0: # Left
-                            if index - 1 >= 0:
-                                if currentRow[index - 1] == '@':
-                                    countRollsAround += 1
-                        case 1: # Top-Left
-                            if index - 1 >= 0:
-                                if previousRow[index - 1] == '@':
-                                    countRollsAround += 1
-                        case 2: # Top-centre
-                            if previousRow[index] == '@':
-                                countRollsAround += 1
-                        case 3: # Top-Right
-                            if index + 1 < len(currentRow):
-                                if previousRow[index + 1] == '@':
-                                    countRollsAround += 1
-                        case 4: # Right
-                            if index + 1 < len(currentRow):
-                                if currentRow[index + 1] == '@':
-                                    countRollsAround += 1
-                        case 5: # Bottom-Right
-                            if index + 1 < len(currentRow):
-                                if nextRow[index + 1] == '@':
-                                    countRollsAround += 1
-                        case 6: # Bottom
-                            if nextRow[index] == '@':
-                                countRollsAround += 1
-                        case 7: # Bottom-Left
-                            if index - 1 >= 0:
-                                if nextRow[index - 1] == '@':
-                                    countRollsAround += 1
-                if countRollsAround < 4:
-                    countOfAccessibleRolls += 1
-                #print(f"Index: {index}, RollsAround: {countRollsAround}, countOfAccessibleRolls: {countOfAccessibleRolls}")
-            previousRow = currentRow
-            currentRow = nextRow
-            print(f"{rowNumber} - Accessible Rolls So Far: {countOfAccessibleRolls}  ")
-            if rowOfRolls == '':
+                allTheRolls.append('.' * lengthOfRow)
                 break
+            else:
+                allTheRolls.append(rowOfRolls)
+            lengthOfRow = len(rowOfRolls)
             rowNumber += 1
-    print(f"Count of Accessible Rolls: {countOfAccessibleRolls}")
+        totalRows = rowNumber
+        # 2 - Loop until no more accessible rolls found
+        while True:
+            accessibleRollsFoundThisPass = 0
+            # 3 - Process each row of rolls
+            for rowOfRolls in range(len(allTheRolls) - 1):
+                if rowOfRolls == 0:
+                    continue # Skip first row as it is all '.'
+                # 4 - For each column in the row
+                for colOfRolls in range(len(allTheRolls[rowOfRolls])): 
+                    if allTheRolls[rowOfRolls][colOfRolls] == '.': # Empty slot so skip to next slot
+                        continue
+                    countRollsAround = 0
+                    for count in range(8):
+                        match count:
+                            case 0: # Left
+                                if colOfRolls - 1 >= 0:
+                                    if allTheRolls[rowOfRolls][colOfRolls - 1] in ['@', 'x']:
+                                        countRollsAround += 1
+                            case 1: # Top-Left
+                                if colOfRolls - 1 >= 0:
+                                    if allTheRolls[rowOfRolls - 1][colOfRolls - 1] in ['@', 'x']:
+                                        countRollsAround += 1
+                            case 2: # Top-centre
+                                if allTheRolls[rowOfRolls - 1][colOfRolls] in ['@', 'x']:
+                                    countRollsAround += 1
+                            case 3: # Top-Right
+                                if colOfRolls + 1 < len(allTheRolls[rowOfRolls - 1]):
+                                    if allTheRolls[rowOfRolls - 1][colOfRolls + 1] in ['@', 'x']:
+                                        countRollsAround += 1
+                            case 4: # Right
+                                if colOfRolls + 1 < len(allTheRolls[rowOfRolls - 1]):
+                                    if allTheRolls[rowOfRolls][colOfRolls + 1] in ['@', 'x']:
+                                        countRollsAround += 1
+                            case 5: # Bottom-Right
+                                if colOfRolls + 1 < len(allTheRolls[rowOfRolls - 1]):
+                                    if allTheRolls[rowOfRolls + 1][colOfRolls + 1] in ['@', 'x']:
+                                        countRollsAround += 1
+                            case 6: # Bottom
+                                if allTheRolls[rowOfRolls + 1][colOfRolls] in ['@', 'x']:
+                                    countRollsAround += 1
+                            case 7: # Bottom-Left
+                                if colOfRolls - 1 >= 0:
+                                    if allTheRolls[rowOfRolls + 1][colOfRolls - 1] in ['@', 'x']:
+                                        countRollsAround += 1
+                    # If the roll is accessible, mark it as an 'x' and increase the count
+                    if countRollsAround < 4:
+                        accessibleRollsFoundThisPass += 1
+                        if colOfRolls == 0:
+                            allTheRolls[rowOfRolls] = 'x' + allTheRolls[rowOfRolls][1:]
+                        elif colOfRolls == len(allTheRolls[rowOfRolls]) - 1:
+                            allTheRolls[rowOfRolls] = allTheRolls[rowOfRolls][0:len(allTheRolls[rowOfRolls]) - 1] + 'x'
+                        else:
+                            allTheRolls[rowOfRolls] = allTheRolls[rowOfRolls][0:colOfRolls] + 'x' + allTheRolls[rowOfRolls][colOfRolls + 1:]
+            totalPasses += 1
+            if accessibleRollsFoundThisPass == 0:
+                break
+            else:
+                countOfAccessibleRolls += accessibleRollsFoundThisPass
+            # Change all the x to . for next pass
+            for rowOfRolls in range(len(allTheRolls) - 1):
+                allTheRolls[rowOfRolls] = allTheRolls[rowOfRolls].replace('x', '.')
+            print(f"Pass {totalPasses} complete. Accessible Rolls found this pass: {accessibleRollsFoundThisPass}. Total so far: {countOfAccessibleRolls}.")
+
+    print(f"Count of Accessible Rolls: {countOfAccessibleRolls} after {totalPasses} passes.")
 
 except Exception as e:
     print(f"Error reading input file. Error: {e}")
